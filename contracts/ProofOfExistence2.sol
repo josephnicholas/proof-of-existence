67143bf0636a699fe2f989553c7ad7aae02760d6
Joseph Nicholas R. Alcantara <josephnicholasalcantara@gmail.com>
2019-12-03 17:18:16 +0800

    Initial commit for POE exercise in consensys

---
+++ contracts/ProofOfExistence2.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.0;
+
+
+contract ProofOfExistence2 {
+  // state
+  bytes32[] private proofs;
+
+  // store a proof of existence in the contract state
+  // *transactional function*
+  function storeProof(bytes32 proof) public {
+    proofs.push(proof);
+  }
+
+  // calculate and store the proof for a document
+  // *transactional function*
+  function notarize(string calldata document) external {
+    bytes32 proof = proofFor(document);
+    storeProof(proof);
+  }
+
+  // helper function to get the document's sha256
+  // *read-only function (pure)*
+  function proofFor(string memory document) public pure returns(bytes32) {
+    return sha256(abi.encodePacked(document));
+  }
+
+  // check if the document has been notarized
+  // *read-only function*
+  function checkDocument(string memory document) view public returns (bool) {
+    bytes32 proof = proofFor(document);
+    return hasProof(proof);
+  }
+
+  // returns true if proof is stored
+  // *read-only function*
+  function hasProof(bytes32 proof) view internal returns (bool) {
+    for(uint256 counter = 0; counter < proofs.length; ++counter) {
+      if(proof == proofs[counter]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ProofOfExistence2 =  artifacts.require("ProofOfExistence2");
+
+module.exports = function(deployer) {
+    deployer.deploy(ProofOfExistence2);
+};
